a862961c3fb69470f40c09b49ca4c000918abdd3
Aj-CreationZ <60123472+Aj-CreationZ@users.noreply.github.com>
2023-08-22 17:50:22 +0530

    AjFizzBuzz.java

--- FizzBuzzProgram/AjFizzBuzz.java
+++ FizzBuzzProgram/AjFizzBuzz.java
@@ -1,3 +1,9 @@
+/*    FizzBuzz Rules
+      For multiples of 3, print "Fizz."
+      For multiples of 5, print "Buzz."
+      For multiples of both 3 and 5, print "FizzBuzz."
+      For all other numbers, simply print the number.  */
+
 public class AjFizzBuzz {
     public static void main(String[] args) {
         for (int i = 1; i <= 100; i++) {
